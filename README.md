bb6ac5d94ba4de378fa40d8664aa5435eed1bb0e
soumallyadev07 <sdev_be18@thapar.edu>
2021-06-11 16:08:21 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# IP Address Tracker
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [My process](#my-process)
+  - [Built with](#built-with)
+
+
+## Overview
+#### A simple flask application for IP Address Tracking and plotting on an interactive map
+
+### The challenge
+
+Users should be able to:
+
+- Login using SAWO sdk (the passwordless way)
+- View the optimal layout for each page depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See their own IP address on the map on the initial page load
+- Search for any IP addresses or domains and see the key information and location
+
+### Screenshot
+
+![IMG1](static/images/Screenshot_1.png)
+![IMG3](static/images/Screenshot_3.png)
+![IMG2](static/images/Screenshot_2.png)
+![IMG3](static/images/Screenshot_4.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- JavaScript
+- Flask
+- Sawo Authentication
+- [IP Geolocation API by IPify](https://geo.ipify.org/)
+- [LeafletJS](https://leafletjs.com/)
+
---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, redirect, url_for, session, request, g
+from functools import wraps
+from sawo import verifyToken
+import requests
+
+app = Flask(__name__)
+app.secret_key = "beufhbe2iufb3efv"
+sawo_key = "3bcfbd15-67dd-4d94-a31c-481caa5ac463"
+map_key = "pk.eyJ1Ijoic291bWFsbHlhZGV2IiwiYSI6ImNrcHI4OWs0ajAzdG0yd281czYxYjhtazcifQ.xtgiSgJ-ihupzlVie34pDA"
+geoip_key = "at_BRwsBp8KuxoFfjMln7iVdV37lr0Fd"
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return redirect(url_for('dashboard'))
+        else:
+            return redirect(url_for('index'))
+
+    return wrap
+
+@app.before_request
+def before_request():
+    g.user = None
+
+    if 'logged_in' in session:
+        user = session['logged_in']
+        g.user = user
+
+@app.route('/')
+def index():
+    return render_template('login.html', sawo_key=sawo_key, map_key=map_key)
+
+
+@app.route('/login', methods=['GET','POST'] )
+def loginfunc():
+    if request.method == 'POST':
+        data = request.get_json()
+        if(verifyToken(data)):
+            #do something
+            session['logged_in'] = data['user_id']
+            return 'OK', 200
+        else:
+            return 'Error', 200
+        
+    
+@app.route('/dashboard')
+def dashboard():
+    if not g.user:
+        return redirect(url_for('index'))
+
+    return render_template('dashboard.html', map_key=map_key, geoip_key=geoip_key)
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
