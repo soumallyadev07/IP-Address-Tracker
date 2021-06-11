79040c1a74e7cc2d08b53a6eeb14e51ab937fefb
soumallyadev07 <sdev_be18@thapar.edu>
2021-06-11 17:18:01 +0530

    404 Page

--- app.py
+++ app.py
@@ -57,5 +57,11 @@ def logout():
     session.clear()
     return redirect(url_for('index'))
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     app.run(debug=False)
